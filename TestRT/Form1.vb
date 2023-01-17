02a2d1a181eeb35e8229172a15a30dcb2f35035f
Kuffs <github@kuffs.co.uk>
2023-01-17 12:21:19 +0000

    Add project files.

---
+++ TestRT/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports BackendlessAPI
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        TestRT.Init()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TestRT.Subscribe()
+    End Sub
+
+    Private Async Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim NewTest = New Test() With {.Name = $"Test{CInt(Random.Shared.NextDouble * 1000)}", .Age = Random.Shared.NextDouble * 1000}
+        Await Backendless.Data.Of(Of Test).SaveAsync(NewTest)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim NewTest = New Test() With {.Name = $"BlockingTest{CInt(Random.Shared.NextDouble * 1000)}", .Age = Random.Shared.NextDouble * 1000}
+        Backendless.Data.Of(Of Test).Save(NewTest)
+        MsgBox("Call has returned")
+    End Sub
+End Class
+
+Public Class Test
+
+    Public Property objectId As String
+    Public Property Name As String
+    Public Property Age As Integer
+End Class
---
+++ TestRT/TestRT.vb
@@ -0,0 +1,44 @@
+﻿Imports BackendlessAPI
+
+
+Public Class TestRT
+
+    Public Shared Sub Init()
+        Backendless.InitApp("4091BABD-BE94-1AF8-FF1C-E1CAB65DA400", "F54E5FC0-137E-45DE-AEA2-69804FD28294")
+    End Sub
+
+    Public Shared Sub Subscribe()
+        Log("Subscribing")
+
+        Dim eventHandler = Backendless.Data.Of("Test").RT
+
+        Backendless.RT.AddConnectListener(New RT.ConnectListener(Sub()
+                                                                     Log("Connected")
+                                                                 End Sub))
+
+        Backendless.RT.AddConnectErrorListener(New RT.ConnectErrorListener(Sub(a)
+                                                                               Log($"Connect Error :{a.Message}")
+                                                                           End Sub))
+
+        Backendless.RT.AddDisconnectListener(New RT.DisconnectListener(Sub(reason)
+                                                                           Log($"Disconnected - {reason}")
+                                                                       End Sub))
+
+        eventHandler.AddCreateListener(Sub(a)
+                                           Log($"Created: {a}")
+                                       End Sub)
+
+        eventHandler.AddUpdateListener(Sub(a)
+                                           Log($"Updated: {a}")
+                                       End Sub)
+
+
+        Log("Subscribed")
+
+    End Sub
+
+    Private Shared Sub Log(text As String)
+        Debug.WriteLine($"{Date.Now:F} {text}")
+    End Sub
+
+End Class
